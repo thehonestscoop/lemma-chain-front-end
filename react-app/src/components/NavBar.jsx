2457a4c18c65f6ce5ab176ef14808ad15b465a0e
Ikechukwu <iykekings36@gmail.com>
2019-07-02 22:37:18 +0100

    styled navbar to fit new design

--- react-app/src/components/NavBar.jsx
+++ react-app/src/components/NavBar.jsx
@@ -1,32 +1,55 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const NavBar = () => {
   return (
     <Nav>
-        <NavLink to="/create-ref">
-          <span>CR</span> Create Ref
-        </NavLink>
-        <NavLink to="/create-account">
-         <span>CA</span> Create Account
-        </NavLink>
+      <NavLink to="/create-ref">
+        <span>CR</span> Create Ref
+      </NavLink>
+      <NavLink to="/create-account">
+        <span>CA</span> Create Account
+      </NavLink>
     </Nav>
   );
 };
 const Nav = styled.nav`
-      background: #3e4061;
-      padding: 1.5rem 0;
-      display: flex;
-      flex-direction: column;
+  background: #3e4061;
+  padding: 1.5rem 0;
+  display: flex;
+  flex-direction: column;
 
-      a {
-        padding: 1rem;
+  a {
+    padding: 1rem;
     border-bottom: 1px solid #80808059;
-    min-width: 200px;
-    color: white;
+    min-width: 250px;
     font-weight: 600;
-      }
+    color: white;
+    display: flex;
+    align-items: center;
+    &:hover {
+      text-decoration: none;
+    }
+    span {
+      border-radius: 50%;
+      background-color: white;
+      color: #3e4061;
+      font-size: 1.2rem;
+      height: 3rem;
+      display: flex;
+      font-weight: 400;
+      justify-content: center;
+      align-items: center;
+      width: 3rem;
+      margin-right: 0.5rem;
+      box-shadow: 1px 1px 15px #00000036;
+    }
+    &.active span {
+      background-color: #33cc33;
+      color: white;
+    }
+  }
 `;
 
 export default NavBar;
